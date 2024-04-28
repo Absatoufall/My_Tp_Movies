5177d0a56ae4e2dcbbe550972bafe6700af2e03f
Absatoufall <128919144+Absatoufall@users.noreply.github.com>
2024-04-28 02:42:50 +0200

    Mycommit

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,116 +1,113 @@
 const db = require("../models");
 const Movie = db.movies;
 
-// Créer et sauvegarder un nouveau film
+// Ccreation et sauvegarde
 exports.create = (req, res) => {
-  // Validation de la requête
-  if (!req.body.title) {
-    res.status(400).send({ message: "Le titre ne peut pas être vide!" });
-    return;
-  }
+    // Validation de la requête
+    if (!req.body.title) {
+        res.status(400).send({ message: "Le titre ne peut pas être vide!" });
+        return;
+    }
 
-  // Créer un film
-  const movie = new Movie({
-    title: req.body.title,
-    synopsis: req.body.synopsis,
-    dateDeSortie: req.body.dateDeSortie,
-    director: req.body.director,
-    genre: req.body.genre,
-    actors: req.body.actors,
-  });
-
-  // Sauvegarder le film dans la base de données
-  movie
-    .save()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Une erreur s'est produite lors de la création du film.",
-      });
+    // creation film 
+    const movie = new Movie({
+        title: req.body.title,
+        synopsis: req.body.synopsis,
+        dateDeSortie: req.body.dateDeSortie,
+        director: req.body.director,
+        genre: req.body.genre,
+        actors: req.body.actors,
     });
+
+    // Enregistrement du film dans la BDD
+    movie
+        .save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Une erreur s'est produite lors de la création du film.",
+            });
+        });
 };
 
-// Récupérer tous les films depuis la base de données
+// Recuperation des films de la base de données
 exports.findAll = (req, res) => {
-  const title = req.query.title;
-  let condition = title
-    ? { title: { $regex: new RegExp(title), $options: "i" } }
-    : {};
-  Movie.find(condition)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Une erreur s'est produite lors de la récupération des films.",
-      });
-    });
+    const title = req.query.title;
+    let condition = title ?
+        { title: { $regex: new RegExp(title), $options: "i" } } :
+        {};
+    Movie.find(condition)
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Une erreur s'est produite lors de la récupération des films.",
+            });
+        });
 };
 
-// Supprimer un film avec l'ID spécifié dans la requête
+// Suppression d'un film avec son ID 
 exports.delete = (req, res) => {
-  const id = req.params.id;
-  Movie.findByIdAndDelete(id)
-    .then((data) => {
-      if (!data) {
-        res.status(404).send({
-          message: `Impossible de supprimer le film avec l'ID=${id}. Peut-être que le film n'a pas été trouvé!`,
+    const id = req.params.id;
+    Movie.findByIdAndDelete(id)
+        .then((data) => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Impossible de supprimer le film avec l'ID=${id}. Peut-être que le film n'a pas été trouvé!`,
+                });
+            } else {
+                res.send({
+                    message: "Le film a été supprimé avec succès!",
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: "Impossible de supprimer le film avec l'ID=" + id,
+            });
         });
-      } else {
-        res.send({
-          message: "Le film a été supprimé avec succès!",
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: "Impossible de supprimer le film avec l'ID=" + id,
-      });
-    });
 };
 
-// Supprimer tous les films de la base de données
+// Suppression de tous les films figurant dans la BDD
 exports.deleteAll = (req, res) => {
-  Movie.deleteMany({})
-    .then((data) => {
-      res.send({
-        message: `${data.deletedCount} films ont été supprimés avec succès!`,
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Une erreur s'est produite lors de la suppression de tous les films.",
-      });
-    });
+    Movie.deleteMany({})
+        .then((data) => {
+            res.send({
+                message: `${data.deletedCount} films ont été supprimés avec succès!`,
+            });
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Une erreur s'est produite lors de la suppression de tous les films.",
+            });
+        });
 };
 
-// Mettre à jour un film par ID
+// Mettre à jour un film par son ID
 exports.update = (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  if (!req.body) {
-    return res.status(400).send({
-      message: "Les données à mettre à jour ne peuvent pas être vides!"
-    });
-  }
-
-  Movie.findByIdAndUpdate(id, req.body, { new: true })
-    .then(movie => {
-      if (!movie) {
-        return res.status(404).send({
-          message: `Film non trouvé avec l'ID ${id}.`
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Les données à mettre à jour ne peuvent pas être vides!"
         });
-      }
-      res.send(movie);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: `Erreur lors de la mise à jour du film avec l'ID ${id}.`
-      });
-    });
+    }
+
+    Movie.findByIdAndUpdate(id, req.body, { new: true })
+        .then(movie => {
+            if (!movie) {
+                return res.status(404).send({
+                    message: `Film non trouvé avec l'ID ${id}.`
+                });
+            }
+            res.send(movie);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Erreur lors de la mise à jour du film avec l'ID ${id}.`
+            });
+        });
 };
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,22 +1,21 @@
 module.exports = app => {
     const movies = require("../controllers/movie.controller.js"); // Modifié de student.controller.js à movie.controller.js
     let router = require("express").Router();
-  
-    // Create a new Movie
-    router.post("/", movies.create); 
-  
-    // Retrieve all Movies
-    router.get("/", movies.findAll); 
-  
-    // Delete a Movie with id
-    router.delete("/:id", movies.delete); 
-    
-    // Delete all movies
-    router.delete("/", movies.deleteAll); 
 
-    // Mettre à jour un film par ID
+    // creer un nouveau film
+    router.post("/", movies.create);
+
+    // Retourner tous les films
+    router.get("/", movies.findAll);
+
+    // Supprimer un film avec  un id
+    router.delete("/:id", movies.delete);
+
+    // Supprimer tous les films
+    router.delete("/", movies.deleteAll);
+
+    // Mettre à jour un film par son id 
     router.put("/:id", movies.update);
-  
-    app.use("/api/movies", router); 
-  };
-  
+
+    app.use("/api/movies", router);
+};
